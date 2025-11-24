bd091b3be3b3ac641a61cecdc633532009307fe5
Boris Ivanov <boris.ivanov@enki-tech.net>
2025-11-24 20:19:33 +0100

    hashmap custom struct

--- src/bin/hashmap.rs
+++ src/bin/hashmap.rs
@@ -3,6 +3,15 @@ This example code counts the frequency of each number in the vector.
  */
 use std::collections::{BTreeMap, HashMap};
 
+#[derive(Hash, PartialEq, Debug)]
+struct User{
+    name: String,
+    password: String,
+    email: String,
+}
+
+
+
 fn logic(numbers: Vec<i32>) -> Vec<(i32, u32)> {
     let mut frequencies = HashMap::new();
 
@@ -31,4 +40,25 @@ fn main() {
         "The frequency of each number in the vector is: {:?}",
         result
     );
+
+
+    let mut user_hm: HashMap<String, User> = HashMap::new();
+
+    user_hm.insert("james_bond".to_string(), User{
+        name: "Bond".to_string(),
+        password: "007".to_string(),
+        email: "007@mi6.gov.uk".to_string(),
+    });
+
+    user_hm.insert("Isaev".to_string(), User{
+        name: "Max Otto von Stierlitz".to_string(),
+        password: "hitlerkaput".to_string(),
+        email: "stierlitz@deutsch.reich".to_string(),
+    });
+
+    user_hm.iter().for_each(|(u, v)|{
+        println!("User: {}", &u);
+        println!("name: {} pass {} email {}", &v.name, &v.password, &v.email);
+        println!("--------------");
+    });
 }
